40f8bc16b84e08839df2592a657bd617e0c5cae8
Rudge Santos <rudges86@gmail.com>
2025-06-16 02:37:13 -0300

    Ajustes de swagger

--- src/main/java/com/emailsender/demo/domain/model/MailSendDTO.java
+++ src/main/java/com/emailsender/demo/domain/model/MailSendDTO.java
@@ -1,8 +1,19 @@
 package com.emailsender.demo.domain.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-public record MailSendDTO(@NotBlank(message = "O Corpo não deve ser vazio.") String body,
+@Schema(description = "Detalhes de requisição de um e-mail")
+public record MailSendDTO(@NotBlank(message = "O Corpo não deve ser vazio.")
+                          @Schema(description = "Exemplo de corpo do e-mail", example = "E-mail enviado",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
+                          String body,
                           @Email(message = "Formato do e-mail está inválido",
-                                  regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$") String to,
-                          @NotBlank(message = "O Assunto não deve ser vazio.") String subject) {}
+                                  regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+                          @Schema(description = "Exemplo destinatário", example = "destinatario@gmail.com",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
+                          String to,
+                          @NotBlank(message = "O Assunto não deve ser vazio.")
+                          @Schema(description = "Exemplo de assunto", example = "Exemplo de e-mail.",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
+                          String subject) {}
--- src/main/java/com/emailsender/demo/domain/model/ResponseDTO.java
+++ src/main/java/com/emailsender/demo/domain/model/ResponseDTO.java
@@ -1,14 +1,21 @@
 package com.emailsender.demo.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Resposta da Api")
 public class ResponseDTO {
+    @Schema(description = "Mensagem de retorno", example = "E-mail enviado com sucesso")
     private String message;
+    @Schema(description = "Código de status da operação realizada", example = "200")
+
     private HttpStatus statusCode;
 
+    @Schema(description = "Lista de mensagens de erro específicas",
+            example = "[\"Formato do e-mail está inválido\", \"O Assunto não deve ser vazio\"]")
     private List<String> listaErros;
 
     public ResponseDTO(String message, HttpStatus statusCode) {
